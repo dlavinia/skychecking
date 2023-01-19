9d9e5fe5929041fb2d69f746b9e575426d83d9d4
dlavinia <dlavinia2003@gmail.com>
2023-01-18 22:18:31 -0300

    creating the base

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <FooterApp></FooterApp>
-  <Card></Card>
+
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 18 ? 'warm' : ''">
+  
+    <main>
+      <div class="search-box">
+        <input 
+        type="text"  
+        class="search-bar" 
+        placeholder="Search..."
+        v-model = "query"
+        @keypress = "fetchWeather"
+        />
+      </div>
+
+      <div class="weather-wrap" v-if ="typeof weather.main != 'undefined' " >
+
+        <div class="location-box">
+          <div class="location"> {{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date"> {{ dateBuilder() }}</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp"> {{ Math.round(weather.main.temp)}} °C</div>
+          <div class="weather"> {{ weather.weather[0].description}}</div>
+        </div>
+      </div>
+    </main>
+
+  </div>
 </template>
 
 <script>
-import FooterApp from './components/FooterApp.vue';
-import Card from './components/Card.vue';
+
+
 export default {
-  name: 'App',
-  components: {
-    Card,
-    FooterApp
+
+  name: 'app',
+  data(){
+    return{
+      api_key: '465af90b1540b9ff8c6e6e5aa5fe175d',
+       url_base: 'https://api.openweathermap.org/data/2.5/',
+       query: '',
+       weather: {}
+    }
+  },
+
+  methods:{
+
+    fetchWeather (e){
+      if(e.key == "Enter"){
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}&lang=pt_br`)
+        .then(res => {
+          return res.json();
+        }).then(this.setResults);
+      }
+    },
+    setResults(results){
+      this.weather = results;
+    },
+    dateBuilder(){
+      let d = new Date();
+      let months = ["Janeiro",  "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+      let days = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year =d.getFullYear();
+
+      return `${day}, ${date} de ${month} de ${year}`;
+    }
   }
+  
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "@/global.css";
+
 </style>
